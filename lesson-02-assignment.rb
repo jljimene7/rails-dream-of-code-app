af465234d39c25aa3a93e1e8c0404db6ecbccf6a
jljimene7 <jljimene96@gmail.com>
2025-08-11 00:13:33 -0400

    task 3 - assigment#2

--- lesson-02-assignment.rb
+++ lesson-02-assignment.rb
@@ -1,3 +1,4 @@
+################################## LESSON WEEK 2################################
 # ---------------------------------------------------------------------------- #
 # Task 2: Add courses, enrollments and mentors assigments for upcoming trimester
 # ---------------------------------------------------------------------------- #
@@ -10,3 +11,20 @@ spring2025.application_deadline
 puts "Year: #{spring2025.year}"
 puts "Term: #{spring2025.term}"
 puts "Application Deadline: #{spring2025.application_deadline}"
+
+# ---------------------------------------------------------------------------- #
+# Task 3: Change the application deadline on the upcoming trimester to give
+#         students more time to apply
+# ---------------------------------------------------------------------------- #
+
+# The trimester instance is an in-memory copy of the data in the row of the
+# database table for that trimester
+trimester = Trimester.find_by(term: 'Fall', year: '2025')
+puts trimenster.application_deadline
+
+# Changing the values on Trimester and then call .save! to save those values in the database
+trimester.application_deadline = '2025-08-25'
+trimester.save!
+
+# Call the .update! method and pass a hash of attributes to update
+trimester.update(application_deadline: '2025-8-25')
