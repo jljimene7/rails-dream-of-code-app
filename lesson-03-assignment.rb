d99bf37587b7d13f62f2d185dd2f2c07c58137e8
Luis Jimenez <jljimene96@gmail.com>
2025-08-31 16:33:08 -0400

    lesson3 first commit

---
+++ lesson-03-assignment.rb
@@ -0,0 +1,7 @@
+################################## LESSON WEEK 3 ################################
+
+# Lets create the migration file for our new submission table 
+# Let's use the generator mentioned in the Rails Guides to create an empty migration file
+
+bin/rails generate migration 
+
